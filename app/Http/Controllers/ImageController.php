6f05bbd3a8329307537a4afee520044d19b9ba7e
hessou <hesmed45@gmail.com>
2025-05-11 14:12:35 +0200

    reverse imges

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,7 +12,7 @@ class ImageController extends Controller
      */
     public function index()
     {
-        $images= Image::all();
+        $images= Image::orderBy('id','desc')->get();
         return response()->json($images,201);
     }
 
